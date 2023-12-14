642099d423dd243c5ed3c233eae369f825d80de7
Cashkid500 <Murtadhob@gmail.com>
2023-12-14 15:41:53 +0100

    Created a variable constants for assets to ensure code readability and maintainability

--- lib/constants/asset_path.dart
+++ lib/constants/asset_path.dart
@@ -7,5 +7,9 @@ class DoctorHuntAssetsPath {
   static const logo = 'assets/images/Logo.png';
 
   // Onboarding Images
-
+  static const ellipse = 'assets/images/Ellipse.png';
+  static const ellipse2 = 'assets/images/Ellipse2.png';
+  static const onboarding1 = 'assets/images/Onboarding1.png';
+  static const onboarding2 = 'assets/images/Onboarding2.png';
+  static const onboarding3 = 'assets/images/Onboarding3.png';
 }
