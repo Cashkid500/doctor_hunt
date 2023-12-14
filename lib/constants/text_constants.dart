606d999378ec334285965648e6efcca212e151a1
Cashkid500 <Murtadhob@gmail.com>
2023-12-14 15:43:27 +0100

    Created a variable constants for texts to ensure code readability and maintainability

--- lib/constants/text_constants.dart
+++ lib/constants/text_constants.dart
@@ -1,10 +1,15 @@
 class DoctorHuntText {
 
 // Splash Screen Text
-  static const String doctorHunt = "Doctor Hunt";
+static const String doctorHunt = "Doctor Hunt";
 
 // Onboarding Text
-
+static const String onboarding1 = "Find Trusted Doctors";
+static const String onboarding2 = "Choose Best Doctors";
+static const String onboarding3 = "Easy Appointments";
+static const String dummyText = "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of it over 2000 years old.";
+static const String getStarted = "Get Started";
+static const String skip = "Skip";
 // Signup Text
 
 
