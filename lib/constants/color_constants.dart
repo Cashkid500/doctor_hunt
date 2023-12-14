154cbced8ab960b185028ba3b321d73a0b66d5e2
Cashkid500 <Murtadhob@gmail.com>
2023-12-14 15:42:28 +0100

    Created a variable constants for colours to ensure code readability and maintainability

--- lib/constants/color_constants.dart
+++ lib/constants/color_constants.dart
@@ -3,3 +3,5 @@ import 'package:flutter/material.dart';
 Color blackText = const Color(0xff000000);
 Color whiteText = const Color(0xffFFFFFF);
 Color blackOut = const Color(0xff222222);
+Color greenTeal = const Color(0xff0EBE7F);
+Color royalIntrigue = const Color(0xff677294);
