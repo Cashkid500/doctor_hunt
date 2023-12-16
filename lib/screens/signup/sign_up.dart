49a94dbf0c8bb1954b6e649ce2b49f6c01a5ad2d
Balogun Murtadho <murtadhob@gmail.com>
2023-12-16 12:24:17 +0100

    Work in progress

--- lib/screens/signup/sign_up.dart
+++ lib/screens/signup/sign_up.dart
@@ -1,4 +1,7 @@
+import 'package:doctor_hunt/constants/asset_path.dart';
+import 'package:doctor_hunt/constants/color_constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SignupScreen extends StatefulWidget {
   const SignupScreen({Key? key}) : super(key: key);
@@ -10,13 +13,122 @@ class SignupScreen extends StatefulWidget {
 class _SignupScreenState extends State<SignupScreen> {
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-          ],
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Center(
+                child: Text("Join us to start searching",
+                    style: TextStyle(
+                      fontSize: 24.sp,
+                      fontWeight: FontWeight.w600,
+                      color: blackText,
+                      fontFamily: DoctorHuntAssetsPath.doctorHuntFont,
+                    ),
+                  ),
+              ),
+              SizedBox(height: 10.sp),
+              SizedBox(
+                width: 299.sp,
+                child: Text("You can search course, apply course and find scholarship for abroad studies",
+                style: TextStyle(
+                  fontSize: 14.sp,
+                  fontWeight: FontWeight.w400,
+                  height: 1.8.sp,
+                  color: royalIntrigue,
+                  fontFamily: DoctorHuntAssetsPath.doctorHuntFont,
+                ),
+                ),
+              ),
+              SizedBox(height: 60.sp),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  
+                  // Google
+                  Container(
+                    width: 150.sp,
+                    height: 45.sp,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12.sp),
+                      color: whiteText,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Image.asset(
+                          DoctorHuntAssetsPath.google
+                        ),
+                        Text(
+                          "Google",
+                          style: TextStyle(
+                            fontSize: 16.sp,
+                            fontWeight: FontWeight.w300,
+                            color: royalIntrigue,
+                            fontFamily: DoctorHuntAssetsPath.doctorHuntFont
+                          ),
+                        ),
+                      ]
+                    ),
+                  ),
+                  
+                  // Facebook
+                  Container(
+                    width: 150.sp,
+                    height: 45.sp,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12.sp),
+                      color: whiteText,
+                    ),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Image.asset(DoctorHuntAssetsPath.facebook),
+                          Text(
+                            "Facebook",
+                            style: TextStyle(
+                                fontSize: 16.sp,
+                                fontWeight: FontWeight.w300,
+                                color: royalIntrigue,
+                                fontFamily:
+                                    DoctorHuntAssetsPath.doctorHuntFont),
+                          ),
+                        ]),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20.sp),
+              SizedBox(
+                height: 54.sp,
+                width: 320.sp,
+                child: TextField(
+                  keyboardType: TextInputType.name,
+                  decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12.r)),
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: 20.sp, vertical: 20.sp),
+                    isCollapsed: true,
+                    hintText: "Name",
+                    hintStyle: TextStyle(
+                      color: royalIntrigue,
+                      fontFamily: DoctorHuntAssetsPath.doctorHuntFont,
+                      fontSize: 16.sp,
+                      fontWeight: FontWeight.w300,
+                    ),
+                    filled: true,
+                    fillColor: whiteText,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.r),
+                      borderSide: BorderSide(color: royalIntrigue, width: 1.sp),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
