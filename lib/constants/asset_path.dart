97a64127cc58d63308a3b9e9d4bad385d181932f
Balogun Murtadho <murtadhob@gmail.com>
2023-12-16 12:23:27 +0100

    Created a variable constants for images to ensure code readibility and maintainability

--- lib/constants/asset_path.dart
+++ lib/constants/asset_path.dart
@@ -12,4 +12,6 @@ class DoctorHuntAssetsPath {
   static const onboarding1 = 'assets/images/Onboarding1.png';
   static const onboarding2 = 'assets/images/Onboarding2.png';
   static const onboarding3 = 'assets/images/Onboarding3.png';
+  static const google = 'assets/images/Google.png';
+  static const facebook = 'assets/images/Facebook.png';
 }
