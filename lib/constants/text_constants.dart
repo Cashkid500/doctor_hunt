2580d9741740b83ffd4b9fb17b6ff7211d3dbfb8
Balogun Murtadho <murtadhob@gmail.com>
2023-12-17 11:18:40 +0100

    Created a variable constants for texts for code readability and maintainability

--- lib/constants/text_constants.dart
+++ lib/constants/text_constants.dart
@@ -1,20 +1,35 @@
 class DoctorHuntText {
-
 // Splash Screen Text
-static const String doctorHunt = "Doctor Hunt";
+  static const String doctorHunt = "Doctor Hunt";
 
 // Onboarding Text
-static const String onboarding1 = "Find Trusted Doctors";
-static const String onboarding2 = "Choose Best Doctors";
-static const String onboarding3 = "Easy Appointments";
-static const String dummyText = "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of it over 2000 years old.";
-static const String getStarted = "Get Started";
-static const String skip = "Skip";
+  static const String onboarding1 = "Find Trusted Doctors";
+  static const String onboarding2 = "Choose Best Doctors";
+  static const String onboarding3 = "Easy Appointments";
+  static const String dummyText =
+      "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of it over 2000 years old.";
+  static const String getStarted = "Get Started";
+  static const String skip = "Skip";
 // Signup Text
-
+  static const String join = "Join us to start searching";
+  static const String search =
+      "You can search course, apply course and find scholarship for abroad studies";
+  static const String google = "Google";
+  static const String facebook = "Facebook";
+  static const String name = "Name";
+  static const String email = "Email";
+  static const String password = "Password";
+  static const String agree =
+      "I agree with the Terms of Service & Privacy Policy";
+  static const String signup = "Sign Up";
+  static const String accountConfirmation = "Have an account? Log in";
 
 // Login Text
-
+  static const String welcomeBack = "Welcome back";
+  static const String emailAddress = "itsmemamun1@gmail.com";
+  static const String login = "Login";
+  static const String forgotPassword = "Forgot password";
+  static const String joinUs = "Don’t have an account? Join us";
 
 // Forgot Password Text
 }
