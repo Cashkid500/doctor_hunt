2836024a58478671f109215512f0a077311a5cfb
Balogun Murtadho <murtadhob@gmail.com>
2023-12-18 10:05:00 +0100

    Created a variable constants for texts for code readability and maintainability

--- lib/constants/text_constants.dart
+++ lib/constants/text_constants.dart
@@ -10,6 +10,7 @@ class DoctorHuntText {
       "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of it over 2000 years old.";
   static const String getStarted = "Get Started";
   static const String skip = "Skip";
+
 // Signup Text
   static const String join = "Join us to start searching";
   static const String search =
@@ -30,6 +31,19 @@ class DoctorHuntText {
   static const String login = "Login";
   static const String forgotPassword = "Forgot password";
   static const String joinUs = "Don’t have an account? Join us";
+  static const String continue1 = "Continue";
+  static const String updatePassword = "Update Password";
+  static const String resetPassword = "Reset Password";
+  static const String newPassword = "New Password";
+  static const String reEnter = "Re-enter Password";
+  static const String setPassword =
+      "Set the new password for your account so you can login and access all the features.";
+  static const String fourDigits =
+      "Enter the 4 digits code that you received in your email.";
+  static const String digitsCode = "Enter 4 Digits Code";
+  static const String verification =
+      "Enter your email for the verification proccesss, we will send 4 digits code to your email.";
+  
 
 // Forgot Password Text
 }
