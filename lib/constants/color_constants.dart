ef6f7c1629da1216f525ecfb5fe845fbd77c711b
Balogun Murtadho <murtadhob@gmail.com>
2023-12-18 02:05:23 +0100

    Created a variable constants for colours for code readability and maintainability

--- lib/constants/color_constants.dart
+++ lib/constants/color_constants.dart
@@ -5,3 +5,4 @@ Color whiteText = const Color(0xffFFFFFF);
 Color blackOut = const Color(0xff222222);
 Color greenTeal = const Color(0xff0EBE7F);
 Color royalIntrigue = const Color(0xff677294);
+Color weatheredStone = const Color(0xffC4C4C4);
