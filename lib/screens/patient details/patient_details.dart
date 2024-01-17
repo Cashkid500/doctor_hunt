95c8f688c478e06827fa75ece3e3679416c290ce
Balogun Murtadho <murtadhob@gmail.com>
2024-01-17 06:59:02 +0100

    Patient details screen completed

---
+++ lib/screens/patient details/patient_details.dart
@@ -0,0 +1,87 @@
+import 'package:doctor_hunt/constants/asset_path.dart';
+import 'package:doctor_hunt/constants/color_constants.dart';
+import 'package:doctor_hunt/constants/text_constants.dart';
+import 'package:doctor_hunt/screens/medical%20record/medical_record.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class PatientDetailsScreen extends StatefulWidget {
+  const PatientDetailsScreen({super.key});
+
+  @override
+  State<PatientDetailsScreen> createState() => _PatientDetailsScreenState();
+}
+
+class _PatientDetailsScreenState extends State<PatientDetailsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                const Color.fromARGB(255, 166, 202, 167), // Top
+                whiteText, // Center
+                const Color.fromARGB(255, 166, 202, 167), // Bottom
+              ],
+            ),
+          ),
+          child: SafeArea(
+            child: Padding(
+              padding: EdgeInsets.only(left: 20.sp),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    // Back Arrow
+                    RowWidget(rowText: DoctorHuntText.patientDetails),
+
+                    SizedBox(height: 80.sp),
+
+                    // Circular Box
+                    Container(
+                      height: 214.sp,
+                      width: 214.sp,
+                      decoration: BoxDecoration(
+                        color: deathVictorious,
+                        borderRadius: BorderRadius.circular(100.r),
+                      ),
+                      child: Image.asset(DoctorHuntAssetsPath.details),
+                    ),
+
+                    SizedBox(height: 50.sp),
+
+                    // Text
+                    Text(
+                      DoctorHuntText.emptyCart,
+                      style: TextStyle(
+                          fontSize: 20.sp,
+                          fontWeight: FontWeight.w700,
+                          fontFamily: DoctorHuntAssetsPath.doctorHuntFont,
+                          color: blackText),
+                    ),
+
+                    SizedBox(height: 30.sp),
+
+                    // Button
+                    ElevatedButtonWidget(
+                      textPath: DoctorHuntText.addTests,
+                      onPressed: () {
+                        // Navigator.of(context).push(MaterialPageRoute(
+                        //   builder: (BuildContext context) =>
+                        //       const AddRecordScreen(),
+                        // ));
+                      },
+                    ),
+                  ]),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
