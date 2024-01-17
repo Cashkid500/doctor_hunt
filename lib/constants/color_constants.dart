7cabef85196e2d4ae32aaab5111364330ffa0b12
Cashkid500 <murtadhob@gmail.com>
2024-01-17 17:13:07 +0100

    Created a variable constant for colors to ensure code readability and maintainability

--- lib/constants/color_constants.dart
+++ lib/constants/color_constants.dart
@@ -7,3 +7,5 @@ Color greenTeal = const Color(0xff0EBE7F);
 Color royalIntrigue = const Color(0xff677294);
 Color weatheredStone = const Color(0xffC4C4C4);
 Color deathVictorious = const Color(0xffC6EFE5);
+Color skyline = const Color(0xff959CB4);
+Color carbon = const Color(0xff333333);
