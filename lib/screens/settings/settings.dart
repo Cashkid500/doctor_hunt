70be3c5913316394ce52a13997e5a110fa91dae6
Cashkid500 <murtadhob@gmail.com>
2024-01-18 11:20:22 +0100

    Setting screen completed

--- lib/screens/settings/settings.dart
+++ lib/screens/settings/settings.dart
@@ -1,7 +1,6 @@
 import 'package:doctor_hunt/constants/asset_path.dart';
 import 'package:doctor_hunt/constants/color_constants.dart';
 import 'package:doctor_hunt/constants/text_constants.dart';
-import 'package:doctor_hunt/screens/help%20center/help_center.dart';
 import 'package:doctor_hunt/screens/medical%20record/medical_record.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
