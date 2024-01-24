59e69c51cc1eed5e1286b049f0893c3cc2ba706a
Balogun Murtadho <murtadhob@gmail.com>
2024-01-24 04:52:33 +0100

    Halfway done on the profile screen

---
+++ lib/screens/profile/profile.dart
@@ -0,0 +1,127 @@
+import 'package:doctor_hunt/constants/asset_path.dart';
+import 'package:doctor_hunt/constants/color_constants.dart';
+import 'package:doctor_hunt/constants/text_constants.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Container(
+            height: 357.sp,
+            width: 375.sp,
+            decoration: BoxDecoration(
+              color: greenTeal,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(30.r),
+                bottomRight: Radius.circular(30.r),
+              ),
+            ),
+            child: SafeArea(
+              child: Padding(
+                padding: EdgeInsets.only(left: 20.sp, right: 20.sp),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                        Container(
+                          height: 30.sp,
+                          width: 30.sp,
+                          decoration: BoxDecoration(
+                              color: whiteText,
+                              borderRadius: BorderRadius.circular(10.r)),
+                          child: GestureDetector(
+                            onTap: () {
+                            //   Navigator.of(context).push(MaterialPageRoute(
+                            //     builder: (BuildContext context) =>
+                            //         const MedicalRecordScreen(),
+                            //   ));
+                            },
+                            child: Icon(Icons.keyboard_arrow_left,
+                                color: royalIntrigue),
+                          ),
+                        ),
+
+                        SizedBox(width: 20.sp),
+
+                        // Text
+                        Text(
+                          DoctorHuntText.profile,
+                          style: TextStyle(
+                              fontSize: 19.sp,
+                              fontWeight: FontWeight.w600,
+                              fontFamily: DoctorHuntAssetsPath.doctorHuntFont,
+                              color: whiteText),
+                        ),
+                      ]
+                      ),
+                      SizedBox(height: 40.sp),
+                      Text(
+                        DoctorHuntText.profileSetup,
+                        style: TextStyle(
+                            fontSize: 18.sp,
+                            fontWeight: FontWeight.w700,
+                            fontFamily: DoctorHuntAssetsPath.doctorHuntFont,
+                            color: whiteText,
+                        ),
+                      ),
+                      SizedBox(height: 15.sp),
+                      SizedBox(
+                        width: 310.sp,
+                        child: Text(
+                          DoctorHuntText.updateProfile,
+                          style: TextStyle(
+                              fontSize: 15.sp,
+                              fontWeight: FontWeight.w400,
+                              fontFamily: DoctorHuntAssetsPath.doctorHuntFont,
+                              color: whiteText,
+                              height: 1.5.sp,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20.sp),
+                      Center(
+                        child: Stack(
+                          children: [
+                            CircleAvatar(
+                              radius: 60.r,
+                              backgroundImage:
+                                  const AssetImage(DoctorHuntAssetsPath.profilePic),
+                            ),
+                            Positioned(
+                              bottom: 8.sp,
+                              right: -5.sp,
+                              child: Container(
+                                height: 36.sp,
+                                width: 36.sp,
+                                decoration: BoxDecoration(
+                                  color: royalIntrigue,
+                                  borderRadius: BorderRadius.circular(100.r),
+                                ),
+                                child: Image.asset(DoctorHuntAssetsPath.camera)),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ]
+                    ),
+              ),
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
