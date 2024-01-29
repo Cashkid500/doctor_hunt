59cfb4ed87db524b4fcb7e0d45b05aca8e9611ff
Cashkid500 <murtadhob@gmail.com>
2024-01-29 17:33:08 +0100

    All records screen completed

--- lib/screens/medical record/all_records.dart
+++ lib/screens/medical record/all_records.dart
@@ -34,7 +34,7 @@ class _AllRecordScreenState extends State<AllRecordScreen> {
           ),
           child: SafeArea(
             child: Padding(
-              padding: EdgeInsets.only(left: 20.sp, right: 20.sp),
+              padding: EdgeInsets.only(left: 20.sp, right: 20.sp, top: 10.sp),
               child: Column(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
