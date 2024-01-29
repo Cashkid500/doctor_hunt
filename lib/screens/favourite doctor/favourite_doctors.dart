8d876146ac9b367819f377708e3539ebb53362bd
Cashkid500 <murtadhob@gmail.com>
2024-01-29 16:58:00 +0100

    Favourite doctors screen completed

--- lib/screens/favourite doctor/favourite_doctors.dart
+++ lib/screens/favourite doctor/favourite_doctors.dart
@@ -35,7 +35,7 @@ class _FavouriteDoctorsScreenState extends State<FavouriteDoctorsScreen> {
         child: SafeArea(
           child: SingleChildScrollView(
             child: Padding(
-              padding: EdgeInsets.only(left: 20.sp, right: 20.sp),
+              padding: EdgeInsets.only(left: 20.sp, right: 20.sp, top: 10.sp),
               child: Column(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
