7691c362c10c5a51c94a6b5685b5c2c752fcbc41
Cashkid500 <murtadhob@gmail.com>
2024-01-29 17:40:13 +0100

    Settings screen completed

--- lib/screens/settings/settings.dart
+++ lib/screens/settings/settings.dart
@@ -35,7 +35,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
           ),
           child: SafeArea(
             child: Padding(
-              padding: EdgeInsets.only(left: 20.sp, right: 20.sp),
+              padding: EdgeInsets.only(left: 20.sp, right: 20.sp, top: 10.sp),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
