4c11c8517b345808a7fbb8c4ecba14f4038b41be
Balogun Murtadho <murtadhob@gmail.com>
2024-01-31 05:46:22 +0100

    Added a navigation route

--- lib/screens/help center/help_center.dart
+++ lib/screens/help center/help_center.dart
@@ -2,6 +2,7 @@ import 'package:doctor_hunt/constants/asset_path.dart';
 import 'package:doctor_hunt/constants/color_constants.dart';
 import 'package:doctor_hunt/constants/text_constants.dart';
 import 'package:doctor_hunt/screens/diagnostic%20tests/diagnostic_test.dart';
+import 'package:doctor_hunt/screens/doctor%20details/doctor_details.dart';
 import 'package:doctor_hunt/screens/medical%20record/medical_record.dart';
 import 'package:doctor_hunt/screens/medicine%20order/medicine_order.dart';
 import 'package:doctor_hunt/screens/menu/menu.dart';
@@ -154,7 +155,14 @@ class _HelpCenterScreenState extends State<HelpCenterScreen> {
                       height: 30.sp,
                     ),
                     // Seventh Row
-                    ExtractedRowWidget(textPath: DoctorHuntText.healthPlans),
+                    ExtractedRowWidget(textPath: DoctorHuntText.healthPlans,
+                    onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                          builder: (BuildContext context) =>
+                              const DoctorDetailsScreen(),
+                        ));
+                      },
+                    ),
                     SizedBox(
                       height: 30.sp,
                     ),
