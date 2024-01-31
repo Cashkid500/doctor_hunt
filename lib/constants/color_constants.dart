76feb2c0990f59c78c754e2e942fd7c7316f019a
Balogun Murtadho <murtadhob@gmail.com>
2024-01-31 05:43:59 +0100

    Created a variable constants for colors to ensure code readability and maintainability

--- lib/constants/color_constants.dart
+++ lib/constants/color_constants.dart
@@ -20,3 +20,4 @@ Color markerGreen = const Color(0xff07D9AD);
 Color mangoTango = const Color(0xffFE7F44);
 Color fadedSunlight = const Color(0xffFFCF68);
 Color skyan = const Color(0xff61CEFF);
+Color silverback = const Color(0xffCBCBCB);
