c1da146811a9735e7207891b0159da3042a44c97
Cashkid500 <murtadhob@gmail.com>
2024-02-02 18:22:22 +0100

    All records screen completed

--- lib/screens/medical record/all_records.dart
+++ lib/screens/medical record/all_records.dart
@@ -1,8 +1,8 @@
 import 'package:doctor_hunt/constants/asset_path.dart';
 import 'package:doctor_hunt/constants/color_constants.dart';
 import 'package:doctor_hunt/constants/text_constants.dart';
+import 'package:doctor_hunt/screens/medical%20record/add_records.dart';
 import 'package:doctor_hunt/screens/medical%20record/medical_record.dart';
-import 'package:doctor_hunt/screens/medicine%20order/medicine_order.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -39,7 +39,14 @@ class _AllRecordScreenState extends State<AllRecordScreen> {
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
                     // Back Arrow
-                    RowWidget(rowText: DoctorHuntText.allRecords),
+                    RowWidget(rowText: DoctorHuntText.allRecords,
+                    onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                          builder: (BuildContext context) =>
+                              const AddRecordScreen(),
+                        ));
+                      },
+                    ),
 
                     SizedBox(height: 50.sp),
 
@@ -61,7 +68,7 @@ class _AllRecordScreenState extends State<AllRecordScreen> {
                       onPressed: () {
                         Navigator.of(context).push(MaterialPageRoute(
                           builder: (BuildContext context) =>
-                              const MedicineOrderScreen(),
+                              const AddRecordScreen(),
                         ));
                       },
                     ),
