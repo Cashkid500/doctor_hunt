3c663d4f9f9c43de4cd9b7510e6b0d189f7c6ec5
Cashkid500 <murtadhob@gmail.com>
2024-02-02 18:05:34 +0100

    Settings screen completed

--- lib/screens/settings/settings.dart
+++ lib/screens/settings/settings.dart
@@ -2,6 +2,7 @@ import 'package:doctor_hunt/constants/asset_path.dart';
 import 'package:doctor_hunt/constants/color_constants.dart';
 import 'package:doctor_hunt/constants/text_constants.dart';
 import 'package:doctor_hunt/screens/medical%20record/medical_record.dart';
+import 'package:doctor_hunt/screens/navigation/navigation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -40,7 +41,14 @@ class _SettingsScreenState extends State<SettingsScreen> {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   // Back Arrow
-                  RowWidget(rowText: DoctorHuntText.settings),
+                  RowWidget(rowText: DoctorHuntText.settings,
+                  onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                          builder: (BuildContext context) =>
+                              const NavigationScreen(),
+                        ));
+                      },
+                  ),
 
                   SizedBox(height: 20.sp),
 
