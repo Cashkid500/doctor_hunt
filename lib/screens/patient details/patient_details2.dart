005abb8a47fdf5ee737f3cc29f0018f3f7f46301
Cashkid500 <murtadhob@gmail.com>
2024-02-02 18:06:40 +0100

    Patient details screen completed

--- lib/screens/patient details/patient_details2.dart
+++ lib/screens/patient details/patient_details2.dart
@@ -1,7 +1,9 @@
 import 'package:doctor_hunt/constants/asset_path.dart';
 import 'package:doctor_hunt/constants/color_constants.dart';
 import 'package:doctor_hunt/constants/text_constants.dart';
+import 'package:doctor_hunt/screens/doctors/doctors.dart';
 import 'package:doctor_hunt/screens/medical%20record/medical_record.dart';
+import 'package:doctor_hunt/screens/patient%20details/patient_details.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -39,7 +41,14 @@ class _PatientDetails2ScreenState extends State<PatientDetails2Screen> {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   // Back Arrow
-                  RowWidget(rowText: DoctorHuntText.patientDetails),
+                  RowWidget(rowText: DoctorHuntText.patientDetails,
+                  onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                          builder: (BuildContext context) =>
+                              const PatientDetailsScreen(),
+                        ));
+                      },
+                  ),
 
                   SizedBox(height: 30.sp),
 
@@ -348,10 +357,10 @@ class _PatientDetails2ScreenState extends State<PatientDetails2Screen> {
                   ElevatedButtonWidget(
                     textPath: DoctorHuntText.continue1,
                     onPressed: () {
-                      // Navigator.of(context).push(MaterialPageRoute(
-                      //   builder: (BuildContext context) =>
-                      //       const PatientDetails2Screen(),
-                      // ));
+                      Navigator.of(context).push(MaterialPageRoute(
+                        builder: (BuildContext context) =>
+                            const DoctorsScreen(),
+                      ));
                     },
                   ),
                   SizedBox(height: 2.sp),
