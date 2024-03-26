553b193d4deddfc7f27fadea3552790bd9a9a8c2
Cashkid500 <murtadhob@gmail.com>
2024-03-26 13:09:04 +0100

    Select time screen completed

--- lib/screens/select time/select_time.dart
+++ lib/screens/select time/select_time.dart
@@ -4,6 +4,7 @@ import 'package:doctor_hunt/constants/text_constants.dart';
 import 'package:doctor_hunt/screens/find%20doctors/find_doctors.dart';
 import 'package:doctor_hunt/screens/medical%20record/medical_record.dart';
 import 'package:doctor_hunt/screens/select%20time/select_time2.dart';
+import 'package:doctor_hunt/screens/select%20time/widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -252,85 +253,3 @@ class _SelectTimeScreenState extends State<SelectTimeScreen> {
   }
 }
 
-class ShrutiKediaContainer extends StatelessWidget {
-  const ShrutiKediaContainer({
-    super.key,
-    required this.isFavorite,
-  });
-
-  final bool isFavorite;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 98.sp,
-      width: 335.sp,
-      decoration: BoxDecoration(
-        color: whiteText,
-        borderRadius: BorderRadius.circular(8.sp),
-        border:
-            Border.all(color: whiteText, width: 1.sp, style: BorderStyle.solid),
-      ),
-      child: Padding(
-        padding: EdgeInsets.only(
-            left: 10.sp, right: 10.sp, top: 10.sp, bottom: 10.sp),
-        child: Row(children: [
-          Image.asset(DoctorHuntAssetsPath.tranquilli),
-          SizedBox(
-            width: 8.sp,
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 10.sp),
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              Row(
-                children: [
-                  //***************** Shruti Kedia *****************
-                  Text(
-                    DoctorHuntText.shruti,
-                    style: TextStyle(
-                      fontSize: 16.sp,
-                      fontWeight: FontWeight.w700,
-                      fontFamily: DoctorHuntAssetsPath.doctorHuntFont,
-                      color: blackText,
-                    ),
-                  ),
-                  SizedBox(
-                    width: 75.sp,
-                  ),
-                  Icon(
-                    !isFavorite ? Icons.favorite_border_sharp : Icons.favorite,
-                    color: !isFavorite ? Colors.grey : Colors.red,
-                    size: 20.sp,
-                  ),
-                ],
-              ),
-
-              SizedBox(
-                height: 5.sp,
-              ),
-
-              // ***************** Upasana *****************
-              Text(
-                DoctorHuntText.upasana,
-                style: TextStyle(
-                  fontSize: 14.sp,
-                  fontWeight: FontWeight.w400,
-                  fontFamily: DoctorHuntAssetsPath.doctorHuntFont,
-                  color: royalIntrigue,
-                ),
-              ),
-
-              SizedBox(
-                height: 5.sp,
-              ),
-
-              //***************** Rating *****************
-              Image.asset(DoctorHuntAssetsPath.rating)
-            ]),
-          ),
-        ]),
-      ),
-    );
-  }
-}
