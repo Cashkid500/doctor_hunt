dea19c842cb6554efd9a6b0d7764ee0fb527f012
Cashkid500 <murtadhob@gmail.com>
2024-03-26 13:49:58 +0100

    Patient details2 screen completed

--- lib/screens/patient details/patient_details2.dart
+++ lib/screens/patient details/patient_details2.dart
@@ -4,6 +4,7 @@ import 'package:doctor_hunt/constants/text_constants.dart';
 import 'package:doctor_hunt/screens/doctors/doctors.dart';
 import 'package:doctor_hunt/screens/medical%20record/medical_record.dart';
 import 'package:doctor_hunt/screens/patient%20details/patient_details.dart';
+import 'package:doctor_hunt/screens/patient%20details/widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -374,65 +375,3 @@ class _PatientDetails2ScreenState extends State<PatientDetails2Screen> {
   }
 }
 
-class Heading extends StatelessWidget {
-  const Heading({
-    super.key,
-    required this.headerPath,
-  });
-
-  final String headerPath;
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      headerPath,
-      style: TextStyle(
-        fontFamily: DoctorHuntAssetsPath.doctorHuntFont,
-        fontSize: 14.sp,
-        fontWeight: FontWeight.w700,
-        color: blackText,
-      ),
-    );
-  }
-}
-
-class FieldWidget extends StatelessWidget {
-  const FieldWidget({
-    super.key,
-    required this.fieldPath,
-  });
-
-  final String fieldPath;
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 54.sp,
-      width: 320.sp,
-      child: TextField(
-        keyboardType: TextInputType.name,
-        decoration: InputDecoration(
-          focusedBorder:
-              OutlineInputBorder(borderRadius: BorderRadius.circular(12.r)),
-          contentPadding:
-              EdgeInsets.symmetric(horizontal: 20.sp, vertical: 20.sp),
-          isCollapsed: true,
-          hintText: fieldPath,
-          hintStyle: TextStyle(
-            color: royalIntrigue,
-            fontFamily: DoctorHuntAssetsPath.doctorHuntFont,
-            fontSize: 16.sp,
-            fontWeight: FontWeight.w300,
-          ),
-          filled: true,
-          fillColor: whiteText,
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(12.r),
-            borderSide: BorderSide(
-                color: Colors.grey, width: 0.1.sp, style: BorderStyle.solid),
-          ),
-        ),
-      ),
-    );
-  }
-}
