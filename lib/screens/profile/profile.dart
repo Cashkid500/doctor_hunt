4686f198c04b1d96b8ceed811b7e320dd6c95090
Cashkid500 <murtadhob@gmail.com>
2024-03-26 13:24:45 +0100

    Profile screen completed

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -4,6 +4,7 @@ import 'package:doctor_hunt/constants/text_constants.dart';
 import 'package:doctor_hunt/screens/medical%20record/medical_record.dart';
 import 'package:doctor_hunt/screens/navigation/navigation.dart';
 import 'package:doctor_hunt/screens/profile/profile2.dart';
+import 'package:doctor_hunt/screens/profile/widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -377,37 +378,4 @@ class _ProfileScreenState extends State<ProfileScreen> {
   }
 }
 
-class ProfileHeader extends StatelessWidget {
-  const ProfileHeader({
-    super.key,
-    this.onTap,
-  });
-  final VoidCallback? onTap;
-  @override
-  Widget build(BuildContext context) {
-    return Row(mainAxisAlignment: MainAxisAlignment.start, children: [
-      Container(
-        height: 30.sp,
-        width: 30.sp,
-        decoration: BoxDecoration(
-            color: whiteText, borderRadius: BorderRadius.circular(10.r)),
-        child: GestureDetector(
-          onTap: onTap,
-          child: Icon(Icons.keyboard_arrow_left, color: royalIntrigue),
-        ),
-      ),
 
-      SizedBox(width: 20.sp),
-
-      //***************** Profile *****************
-      Text(
-        DoctorHuntText.profile,
-        style: TextStyle(
-            fontSize: 19.sp,
-            fontWeight: FontWeight.w600,
-            fontFamily: DoctorHuntAssetsPath.doctorHuntFont,
-            color: whiteText),
-      ),
-    ]);
-  }
-}
