9fe0ccd29be225308eefd9aa30c997d05f8efcf3
Cashkid500 <murtadhob@gmail.com>
2024-03-26 13:01:13 +0100

    Settings screen completed

--- lib/screens/settings/settings.dart
+++ lib/screens/settings/settings.dart
@@ -3,6 +3,7 @@ import 'package:doctor_hunt/constants/color_constants.dart';
 import 'package:doctor_hunt/constants/text_constants.dart';
 import 'package:doctor_hunt/screens/medical%20record/medical_record.dart';
 import 'package:doctor_hunt/screens/navigation/navigation.dart';
+import 'package:doctor_hunt/screens/settings/widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -230,85 +231,3 @@ class _SettingsScreenState extends State<SettingsScreen> {
   }
 }
 
-class MoreOptionsRow extends StatelessWidget {
-  const MoreOptionsRow({
-    super.key,
-    this.onTap,
-    required this.textPath1,
-    required this.textPath2,
-  });
-
-  final String textPath1;
-  final String textPath2;
-  final VoidCallback? onTap;
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-      Text(
-        textPath1,
-        style: TextStyle(
-          fontSize: 16.sp,
-          fontWeight: FontWeight.w400,
-          fontFamily: DoctorHuntAssetsPath.doctorHuntFont,
-          color: royalIntrigue,
-        ),
-      ),
-      Spacer(),
-      Text(
-        textPath2,
-        style: TextStyle(
-          fontSize: 12.sp,
-          fontWeight: FontWeight.w400,
-          fontFamily: DoctorHuntAssetsPath.doctorHuntFont,
-          color: royalIntrigue,
-        ),
-      ),
-      GestureDetector(
-        onTap: onTap,
-        child: Icon(
-          Icons.chevron_right,
-          color: royalIntrigue,
-        ),
-      ),
-    ]);
-  }
-}
-
-class AccountSettingsRow extends StatelessWidget {
-  const AccountSettingsRow({
-    super.key,
-    this.onTap,
-    required this.imagePath,
-    required this.textPath,
-  });
-
-  final String textPath;
-  final String imagePath;
-  final VoidCallback? onTap;
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(children: [
-      Image.asset(imagePath),
-      SizedBox(width: 20.sp),
-      Text(
-        textPath,
-        style: TextStyle(
-          fontSize: 16.sp,
-          fontWeight: FontWeight.w400,
-          fontFamily: DoctorHuntAssetsPath.doctorHuntFont,
-          color: royalIntrigue,
-        ),
-      ),
-      Spacer(),
-      GestureDetector(
-        onTap: onTap,
-        child: Icon(
-          Icons.chevron_right,
-          color: royalIntrigue,
-        ),
-      ),
-    ]);
-  }
-}
