45d0a6a7c2ebd16455dab0f7ec0e239f1ac0781d
Cashkid500 <murtadhob@gmail.com>
2024-03-26 14:53:30 +0100

    Find doctors screen completed

--- lib/screens/find doctors/find_doctors.dart
+++ lib/screens/find doctors/find_doctors.dart
@@ -3,6 +3,7 @@ import 'package:doctor_hunt/constants/color_constants.dart';
 import 'package:doctor_hunt/constants/text_constants.dart';
 import 'package:doctor_hunt/screens/medical%20record/medical_record.dart';
 import 'package:doctor_hunt/screens/my%20doctors/my_doctors.dart';
+import 'package:doctor_hunt/screens/my%20doctors/widget.dart';
 import 'package:doctor_hunt/screens/select%20time/select_time.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
